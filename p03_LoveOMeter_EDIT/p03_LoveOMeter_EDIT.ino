6a75849b61ba2963961671ce63df9906bbd88b1d
Paul MacLachlan <paulmacother@gmail.com>
2019-06-20 07:07:29 -0700

    resolving Arduino saving issues

--- p03_LoveOMeter_EDIT/p03_LoveOMeter_EDIT.ino
+++ p03_LoveOMeter_EDIT/p03_LoveOMeter_EDIT.ino
@@ -1,21 +1,3 @@
-/*
-  Arduino Starter Kit example
-  Project 3 - Love-O-Meter
-
-  This sketch is written to accompany Project 3 in the Arduino Starter Kit
-
-  Parts required:
-  - one TMP36 temperature sensor
-  - three red LEDs
-  - three 220 ohm resistors
-
-  created 13 Sep 2012
-  by Scott Fitzgerald
-
-  http://www.arduino.cc/starterKit
-
-  This example code is part of the public domain.
-*/
 
 // named constant for the pin the sensor is connected to
 const int sensorPin = A0;
