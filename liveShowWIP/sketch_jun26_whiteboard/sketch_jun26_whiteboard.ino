1ffde1afe7da71b833c039bdf14cfa38dd8612f3
Paul MacLachlan <paulmacother@gmail.com>
2019-06-27 08:10:46 -0700

    directory cleanup

---
+++ liveShowWIP/sketch_jun26_whiteboard/sketch_jun26_whiteboard.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  // initialize displays (check motor functions, responses, report results)
+  // detect locations
+  // check all motors for response
+  // run one pass length of track on each display module, report results
+  // 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
